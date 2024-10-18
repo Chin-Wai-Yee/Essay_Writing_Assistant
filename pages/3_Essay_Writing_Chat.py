8cb14371216013de8d0977fdf3773676e8068c75
Chin Wai Yee <124246295+Chin-Wai-Yee@users.noreply.github.com>
2024-10-18 00:49:01 +0000

    change all to gemini

--- pages/3_Essay_Writing_Chat.py
+++ pages/3_Essay_Writing_Chat.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import streamlit as st
+import google.generativeai as genai
 
 st.set_page_config(page_title="Essay Writing Chat", page_icon="💬")
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from Connection import get_openai_connection
+from Connection import get_openai_connection, get_genai_connection
 
 st.write("# Essay Writing Chat 💬")
 
@@ -50,15 +51,44 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+        # stream = client.chat.completions.create(
+        #     model=st.session_state["openai_model"],
+        #     messages=[
+        #         {"role": m["role"], "content": m["content"]}
+        #         for m in st.session_state.messages
+        #     ],
+        #     stream=True,
+        # )
+        
+        # Gemini
+        get_genai_connection()
+        system_prompt = (
+            "You are an essay teacher and will be answering "
+            "only question that related to essay writing. "
+            "You can write a sample essay based on a type or any topic "
+            "or based a given structure. "
+            "If a question is not related to essay writing, "
+            "do not answer it and provide your reasons."
         )
-        response = st.write_stream(stream)
+        model = genai.GenerativeModel(
+            "gemini-1.5-flash",
+            system_instruction = (
+                system_prompt
+            )
+        )
+
+        stream = model.generate_content(
+            [
+                f'role": {m["role"]}, "content": {m["content"]}'
+                for m in st.session_state.messages
+            ]
+        ).text
+
+        st.write(stream)
+        # response = st.write_stream(stream)
     st.session_state.messages.append(
-        {"role": "assistant", "content": response}
+        {"role": "assistant", "content": stream}
     )
+    # st.session_state.messages.append(
+    #     {"role": "assistant", "content": response}
+    # )
